3828108ca79ff05441b74a16944ca77c0b177a1b
Trinity Dozier <trdozier66@gmail.com>
2024-09-29 21:08:20 -0400

    Q2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double winterCost = 200.00; // Example winter maintenance cost
+        double springCost = 150.00; // Example spring maintenance cost
+        double summerCost = 100.00; // Example summer maintenance cost
+        double fallCost = 180.00; // Example fall maintenance cost
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double totalYearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Winter Cost: $" + winterCost);
+        System.out.println("Spring Cost: $" + springCost);
+        System.out.println("Summer Cost: $" + summerCost);
+        System.out.println("Fall Cost: $" + fallCost);
+        System.out.println("Total Yearly Maintenance Cost: $" + totalYearlyCost);
     }
 }
